960d8a1c69d9da6c311af9c2c85a3ca05db3e3b8
Siew Yi Liang <admin@sonictk.com>
2018-07-29 01:45:22 -0700

    add blank entry/exit points for maya plugin

--- maya_python_c_ext_plugin_main.cpp
+++ maya_python_c_ext_plugin_main.cpp
@@ -1,4 +1,23 @@
 #include "maya_python_c_ext_plugin_main.h"
+#include <maya/MFnPlugin.h>
+
+const char *kAUTHOR = "Siew Yi Liang";
+const char *kVERSION = "1.0.0";
+const char *kREQUIRED_API_VERSION = "Any";
 
 
-// TODO: (sonictk) Basic maya plugin setup here
+MStatus initializePlugin(MObject obj)
+{
+	MStatus status;
+	MFnPlugin plugin(obj, kAUTHOR, kVERSION, kREQUIRED_API_VERSION);
+
+	return status;
+}
+
+
+MStatus uninitializePlugin(MObject obj)
+{
+	MStatus status;
+
+	return status;
+}
