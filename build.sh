4052986c7b59d0a43eb46129d1edca45f6adde28
yliangsiew <yliangsiew@blizzard.com>
2018-07-30 11:51:01 -0700

    fixed linux build script to actually work

--- build.sh
+++ build.sh
@@ -4,16 +4,14 @@
 # usage: build.sh [debug|release]
 
 StartTime=`date +%T`;
-echo "Build script started executing at $StartTime...";
+echo "Build script started executing at ${StartTime}...";
 
 # Process command line arguments
 BuildType=$1;
 
 if [ "$BuildType" == "" ]; then
-    BuildType=release;
+    BuildType="release";
 fi;
-
-echo "Building in $BuildType mode ...";
 
 
 # Define colours to be used for terminal output messages
@@ -36,89 +34,91 @@ fi;
 
 
 # Create a build directory to store artifacts
-BuildDir=$PWD/linuxbuild;
-echo "Building in directory: $buildDir";
-if [ ! -d $BuildDir ];
-   mkdir -p $BuildDir;
+BuildDir="${PWD}/linuxbuild";
+echo "Building in directory: $BuildDir";
+if [ ! -d "$BuildDir" ]; then
+   mkdir -p "$BuildDir";
 fi;
 
 
 # Set up globals
-MayaRootDir=/usr/bin/autodesk/maya2018;
-MayaIncludeDir=$MayaRootDir/include;
+MayaRootDir="/usr/bin/autodesk/maya2018";
+MayaIncludeDir="$MayaRootDir/include";
+MayaLibraryDir="$MayaRootDir/lib";
 
-ProjectName=maya_python_c_ext;
+ProjectName="maya_python_c_ext";
 
-MayaPluginEntryPoint=$PWD/$ProjectName_plugin_main.cpp;
-PythonModuleEntryPoint=$PWD/$ProjectName_py_mod_main.cpp;
+MayaPluginEntryPoint="${PWD}/${ProjectName}_plugin_main.cpp";
+PythonModuleEntryPoint="${PWD}/${ProjectName}_py_mod_main.cpp";
 
 # Setup all the compiler flags
-CommonCompilerFlags=-DBits64_ -m64 -DUNIX -D_BOOL -DLINUX -DFUNCPROTO -D_GNU_SOURCE -DLINUX_64 -fPIC -fno-strict-aliasing -DREQUIRE_IOSTREAM -Wall -std=c++11 -Wno-multichar -Wno-comment -Wno-sign-compare -funsigned-char -pthread -Wno-deprecated -Wno-reorder -ftemplate-depth-25 -fno-gnu-keywords -o $PWD/$ProjectName.o;
+CommonCompilerFlags="-DBits64_ -m64 -DUNIX -D_BOOL -DLINUX -DFUNCPROTO -D_GNU_SOURCE -DLINUX_64 -fPIC -fno-strict-aliasing -DREQUIRE_IOSTREAM -Wall -std=c++11 -Wno-multichar -Wno-comment -Wno-sign-compare -funsigned-char -pthread -Wno-deprecated -Wno-reorder -ftemplate-depth-25 -fno-gnu-keywords -o \"${BuildDir}/${ProjectName}.o\"";
 
 # Add the include directories for header files
-CommonCompilerFlags=$CommonCompilerFlags -I$MayaRootDir/include -I$MayaRootDir/include/python2.7;
+CommonCompilerFlags="${CommonCompilerFlags} -I${MayaRootDir}/include -I${MayaRootDir}/include/python2.7";
 
-CommonCompilerFlagsDebug=-ggdb -O0 $CommonCompilerFlags;
-CommonCompilerFlagsRelease=-O3 $CommonCompilerFlags;
+CommonCompilerFlagsDebug="-ggdb -O0 ${CommonCompilerFlags}";
+CommonCompilerFlagsRelease="-O3 ${CommonCompilerFlags}";
 
-MayaPluginCompilerFlagsDebug=$CommonCompilerFlagsDebug $MayaPluginEntryPoint;
-MayaPluginCompilerFlagsRelease=$CommonCompilerFlagsRelease $MayaPluginEntryPoint;
+MayaPluginCompilerFlagsDebug="${CommonCompilerFlagsDebug} ${MayaPluginEntryPoint}";
+MayaPluginCompilerFlagsRelease="${CommonCompilerFlagsRelease} ${MayaPluginEntryPoint}";
 
-PythonModuleCompilerFlagsDebug=$CommonCompilerFlagsDebug $PythonModuleEntryPoint;
-PythonModuleCompilerFlagsRelease=$CommonCompilerFlagsRelease $PythonModuleEntryPoint;
+PythonModuleCompilerFlagsDebug="${CommonCompilerFlagsDebug} ${PythonModuleEntryPoint}";
+PythonModuleCompilerFlagsRelease="${CommonCompilerFlagsRelease} ${PythonModuleEntryPoint}";
 
 # As per the Maya official Makefile:
 # -Bsymbolic binds references to global symbols within the library.
 # This avoids symbol clashes in other shared libraries but forces
 # the linking of all required libraries.
-CommonLinkerFlags=-Wl,-Bsymbolic -shared;
+CommonLinkerFlags="-Wl,-Bsymbolic -shared";
 
 # Add all the Maya libraries to link against
-CommonLinkerFlags=$CommonLinkerFlags $MayaRootDir/liblibOpenMaya.so $MayaRootDir/lib/libOpenMayaAnim.so $MayaRootDir/lib/libOpenMayaFX.so $MayaRootDir/lib/libOpenMayaRender.so $MayaRootDir/lib/libOpenMayaUI.so $MayaRootDir/lib/libFoundation.so $MayaRootDir/lib/libclew.so $MayaRootDir/lib/libOpenMayalib.so $MayaRootDir/lib/libOpenMaya.so $MayaRootDir/lib/libAnimSlice.so $MayaRootDir/lib/libDeformSlice.so $MayaRootDir/lib/libModifiers.so $MayaRootDir/lib/libDynSlice.so $MayaRootDir/lib/libKinSlice.so $MayaRootDir/lib/libModelSlice.so $MayaRootDir/lib/libNurbsSlice.so $MayaRootDir/lib/libPolySlice.so $MayaRootDir/lib/libProjectSlice.so $MayaRootDir/lib/libImage.so $MayaRootDir/lib/libShared.so $MayaRootDir/lib/libTranslators.so $MayaRootDir/lib/libDataModel.so $MayaRootDir/lib/libRenderModel.so $MayaRootDir/lib/libNurbsEngine.so $MayaRootDir/lib/libDependEngine.so $MayaRootDir/lib/libCommandEngine.so $MayaRootDir/lib/libFoundation.so $MayaRootDir/lib/libIMFbase.so;
+CommonLinkerFlags="${CommonLinkerFlags} ${MayaLibraryDir}/libOpenMaya.so ${MayaLibraryDir}/libOpenMayaAnim.so ${MayaLibraryDir}/libOpenMayaFX.so ${MayaLibraryDir}/libOpenMayaRender.so ${MayaLibraryDir}/libOpenMayaUI.so ${MayaLibraryDir}/libFoundation.so ${MayaLibraryDir}/libclew.so ${MayaLibraryDir}/libOpenMayalib.so ${MayaLibraryDir}/libOpenMaya.so ${MayaLibraryDir}/libAnimSlice.so ${MayaLibraryDir}/libDeformSlice.so ${MayaLibraryDir}/libModifiers.so ${MayaLibraryDir}/libDynSlice.so ${MayaLibraryDir}/libKinSlice.so ${MayaLibraryDir}/libModelSlice.so ${MayaLibraryDir}/libNurbsSlice.so ${MayaLibraryDir}/libPolySlice.so ${MayaLibraryDir}/libProjectSlice.so ${MayaLibraryDir}/libImage.so ${MayaLibraryDir}/libShared.so ${MayaLibraryDir}/libTranslators.so ${MayaLibraryDir}/libDataModel.so ${MayaLibraryDir}/libRenderModel.so ${MayaLibraryDir}/libNurbsEngine.so ${MayaLibraryDir}/libDependEngine.so ${MayaLibraryDir}/libCommandEngine.so ${MayaLibraryDir}/libFoundation.so ${MayaLibraryDir}/libIMFbase.so";
 
 # Now add the OS libraries to link against
-CommonLinkerFlags=$CommonLinkerFlags libm libdl;
+CommonLinkerFlags="${CommonLinkerFlags} libm libdl";
 
-CommonLinkerFlagsDebug=$CommonLinkerFlags -ggdb -O0;
-CommonLinkerFlagsRelease=$CommonLinkerFlags -O2;
+CommonLinkerFlagsDebug="${CommonLinkerFlags} -ggdb -O0";
+CommonLinkerFlagsRelease="${CommonLinkerFlags} -O2";
 
-MayaPluginExtension=so;
-PythonModuleExtension=$MayaPluginExtension;
+MayaPluginExtension="so";
+PythonModuleExtension="${MayaPluginExtension}";
 
-MayaPluginLinkerFlagsCommon=-o "$BuildDir/$ProjectName.$MayaPluginExtension";
-PythonModuleLinkerFlagsCommon=-o "$BuildDir/$ProjectName.$PythonModuleExtension";
+MayaPluginLinkerFlagsCommon="-o ${BuildDir}/${ProjectName}.${MayaPluginExtension}";
+PythonModuleLinkerFlagsCommon="-o ${BuildDir}/${ProjectName}.${PythonModuleExtension}";
 
-MayaPluginLinkerFlagsRelease=$CommonLinkerFlagsRelease $CommonLinkerFlags $MayaPluginLinkerFlagsCommon;
-MayaPluginLinkerFlagsDebug=$CommonLinkerFlagsDebug $CommonLinkerFlags $MayaPluginLinkerFlagsCommon;
+MayaPluginLinkerFlagsRelease="${CommonLinkerFlagsRelease} ${CommonLinkerFlags} ${MayaPluginLinkerFlagsCommon}";
+MayaPluginLinkerFlagsDebug="${CommonLinkerFlagsDebug} ${CommonLinkerFlags} ${MayaPluginLinkerFlagsCommon}";
 
-PythonModuleLinkerFlagsRelease=$CommonLinkerFlagsRelease $CommonLinkerFlags $PythonModuleLinkerFlagsCommon;
-PythonModuleLinkerFlagsDebug=$CommonLinkerFlagsDebug $CommonLinkerFlags $PythonModuleLinkerFlagsCommon;
+PythonModuleLinkerFlagsRelease="${CommonLinkerFlagsRelease} ${CommonLinkerFlags} ${PythonModuleLinkerFlagsCommon}";
+PythonModuleLinkerFlagsDebug="${CommonLinkerFlagsDebug} ${CommonLinkerFlags} ${PythonModuleLinkerFlagsCommon}";
 
 
 if [ "$BuildType" == "debug" ]; then
     echo "Building in debug mode...";
 
-    MayaPluginCompilerFlags=$MayaPluginCompilerFlagsDebug;
-    MayaPluginLinkerFlags=$MayaPluginLinkerFlagsDebug;
+    MayaPluginCompilerFlags="${MayaPluginCompilerFlagsDebug}";
+    MayaPluginLinkerFlags="${MayaPluginLinkerFlagsDebug}";
 
-    PythonModuleCompilerFlags=$PythonModuleCompilerFlagsDebug;
-    PythonModuleLinkerFlags=$PythonModuleLinkerFlagsDebug;
+    PythonModuleCompilerFlags="${PythonModuleCompilerFlagsDebug}";
+    PythonModuleLinkerFlags="${PythonModuleLinkerFlagsDebug}";
 else
     echo "Building in release mode...";
 
-    MayaPluginCompilerFlags=$MayaPluginCompilerFlagsRelease;
-    MayaPluginLinkerFlags=$MayaPluginLinkerFlagsRelease;
+    MayaPluginCompilerFlags="${MayaPluginCompilerFlagsRelease}";
+    MayaPluginLinkerFlags="${MayaPluginLinkerFlagsRelease}";
 
-    PythonModuleCompilerFlags=$PythonModuleCompilerFlagsRelease;
-    PythonModuleLinkerFlags=$PythonModuleLinkerFlagsRelease;
+    PythonModuleCompilerFlags="${PythonModuleCompilerFlagsRelease}";
+    PythonModuleLinkerFlags="${PythonModuleLinkerFlagsRelease}";
 fi;
 
 
 # Now build the standalone Python module first
-echo "Compiling Python module (command follows)..."
-echo "g++ $PythonModuleCompilerFlags"
+echo "Compiling Python module (command follows)...";
+echo "g++ ${PythonModuleCompilerFlags}";
+echo "";
 
-g++ $PythonModuleCompilerFlags
+g++ ${PythonModuleCompilerFlags};
 
 if [ $? -ne 0 ]; then
     echo -e "${RED}***************************************${NC}";
@@ -128,10 +128,11 @@ if [ $? -ne 0 ]; then
 fi;
 
 
-echo "Linking Python module (command follows)..."
-echo "ld $PythonModuleLinkerFlags"
+echo "Linking Python module (command follows)...";
+echo "ld ${PythonModuleLinkerFlags}";
+echo "";
 
-ld $PythonModuleLinkerFlags
+ld ${PythonModuleLinkerFlags};
 
 if [ $? -ne 0 ]; then
     echo -e "${RED}***************************************${NC}";
@@ -142,10 +143,11 @@ fi;
 
 
 # Now build the Maya plugin
-echo "Compiling Maya plugin (command follows)..."
-echo "g++ $MayaPluginCompilerFlags"
+echo "Compiling Maya plugin (command follows)...";
+echo "g++ ${MayaPluginCompilerFlags}";
+echo "";
 
-g++ $MayaPluginCompilerFlags
+g++ ${MayaPluginCompilerFlags};
 
 if [ $? -ne 0 ]; then
     echo -e "${RED}***************************************${NC}";
@@ -154,9 +156,11 @@ if [ $? -ne 0 ]; then
     exit 3;
 fi;
 
-echo "Linking (command follows)..."
-echo "ld $MayaPluginLinkerFlags"
-ld $MayaPluginLinkerFlags
+echo "Linking (command follows)...";
+echo "ld ${MayaPluginLinkerFlags}";
+echo "";
+
+ld ${MayaPluginLinkerFlags};
 
 if [ $? -ne 0 ]; then
     echo -e "${RED}***************************************${NC}";
@@ -172,6 +176,6 @@ echo -e "${GREEN}***************************************${NC}";
 
 
 EndTime=`date +%T`;
-echo "Build script finished execution at $EndTime.";
+echo "Build script finished execution at ${EndTime}.";
 
 exit 0;
