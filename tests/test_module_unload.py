ab40b0fcbd6b7cadaf2f4c47954b3b9419977d16
Siew Yi Liang <admin@sonictk.com>
2018-08-02 22:37:32 -0700

    added examples of using the bindings

---
+++ tests/test_module_unload.py
@@ -0,0 +1,22 @@
+import os
+import maya.standalone
+
+if __name__ == '__main__':
+    maya.standalone.initialize()
+    import maya.cmds as cmds
+
+    pluginPath = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'msbuild', 'maya_python_c_ext.mll')
+
+    print('loading plugin from {}'.format(pluginPath))
+
+    cmds.loadPlugin(pluginPath)
+
+    import maya_python_c_ext as mpce
+
+    mpce.hello_world_maya('my string')
+
+    cmds.unloadPlugin('maya_python_c_ext')
+
+    mpce.hello_world_maya('my string')
+
+    maya.standalone.uninitialize()
